0f0f087a6261b58a66b45b97b44e701b991cccf5
Griffin Pendrick <griffinpendrick77@gmail.com>
2025-08-28 19:31:24 -0400

    intro

--- 8-28/main.java
+++ 8-28/main.java
@@ -2,6 +2,10 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		System.out.print("Griffin Pendrick (he/him)\n");
+		System.out.print("Computer Science\n");
+		System.out.print("Personal Projects\n");
+		System.out.print("I like making games.\n");
+		System.out.print("Rockets\n");
 	}
 }
